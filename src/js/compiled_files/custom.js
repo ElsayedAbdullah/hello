5fec18acbe3d3e71bf3b79e7edbd2444b1f1c5af
Elsayed Abdullah <sayedaladway@gmail.com>
2022-07-15 11:45:59 +0200

    working on login page

--- src/js/compiled_files/custom.js
+++ src/js/compiled_files/custom.js
@@ -1,98 +1,4 @@
 $(document).ready(function () {
-  $(window).on("resize", function () {
-    triggerFlickity();
-  });
-
-  function triggerFlickity() {
-    if ($(window).width() > 767) {
-      // Play with this value to change the speed
-      let tickerSpeed = 1;
-
-      let flickity = null;
-      let flickity2 = null;
-      let isPaused = false;
-      const slideshowEl = document.querySelector(".js-slideshow");
-      const slideshowEl2 = document.querySelector(".js-slideshow2");
-
-      //   Functions
-      const update = () => {
-        if (isPaused) return;
-        if (flickity.slides) {
-          flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth;
-          flickity.selectedIndex = flickity.dragEndRestingSelect();
-          flickity.updateSelectedSlide();
-          flickity.settle(flickity.x);
-        }
-        if (flickity2.slides) {
-          flickity2.x = (flickity2.x - tickerSpeed) % flickity2.slideableWidth;
-          flickity2.selectedIndex = flickity2.dragEndRestingSelect();
-          flickity2.updateSelectedSlide();
-          flickity2.settle(flickity2.x);
-        }
-        window.requestAnimationFrame(update);
-      };
-
-      const pause = () => {
-        isPaused = true;
-      };
-
-      const play = () => {
-        if (isPaused) {
-          isPaused = false;
-          window.requestAnimationFrame(update);
-        }
-      };
-
-      //   Create Flickity
-      flickity = new Flickity(slideshowEl, {
-        autoPlay: false,
-        prevNextButtons: false,
-        pageDots: false,
-        draggable: true,
-        wrapAround: true,
-        selectedAttraction: 0.015,
-        friction: 0.25,
-      });
-      flickity.x = 0;
-
-      flickity2 = new Flickity(slideshowEl2, {
-        autoPlay: false,
-        prevNextButtons: false,
-        pageDots: false,
-        draggable: true,
-        wrapAround: true,
-        selectedAttraction: 0.015,
-        friction: 0.25,
-        rightToLeft: true,
-      });
-      flickity2.x = 0;
-
-      //   Add Event Listeners
-      slideshowEl.addEventListener("mouseenter", pause, false);
-      slideshowEl.addEventListener("focusin", pause, false);
-      slideshowEl.addEventListener("mouseleave", play, false);
-      slideshowEl.addEventListener("focusout", play, false);
-
-      slideshowEl2.addEventListener("mouseenter", pause, false);
-      slideshowEl2.addEventListener("focusin", pause, false);
-      slideshowEl2.addEventListener("mouseleave", play, false);
-      slideshowEl2.addEventListener("focusout", play, false);
-
-      flickity.on("dragStart", () => {
-        isPaused = true;
-      });
-
-      flickity2.on("dragStart", () => {
-        isPaused = true;
-      });
-
-      //   Start Ticker
-      update();
-    }
-  }
-
-  triggerFlickity();
-
   // when click in anywhere in document close the navbar menu and clear the overlay from the body
   $("body").on("click", function (e) {
     var $currEl = $(e.currentTarget);
@@ -101,7 +7,7 @@ $(document).ready(function () {
       $("body").removeClass("overlay");
       $(".navbar").removeClass("bg-white");
       $(".navbar-collapse").removeClass("active");
-      $('.download-apps').removeClass('active')
+      $(".download-apps").removeClass("active");
     }
   });
 
@@ -115,14 +21,9 @@ $(document).ready(function () {
 
   // language dropdown
   $(".dropdown-menu a").on("click", function () {
-    $(this)
-      .addClass("active")
-      .parent()
-      .siblings()
-      .find("a")
-      .removeClass("active");
+    $(this).addClass("active").parent().siblings().find("a").removeClass("active");
     // $(".language-dropdown .lang").html($(this).html());
-    $('.dropdown-menu').removeClass('show')
+    $(".dropdown-menu").removeClass("show");
   });
 
   // $('.login').on('click', function() {
@@ -152,7 +53,7 @@ $(document).ready(function () {
 
   $(document).click(function () {
     $("#dropdown-menu,#dropdown-menu2").removeClass("show");
-    $('.download-apps').removeClass('active')
+    $(".download-apps").removeClass("active");
   });
 
   /* Clicks within the dropdown won't make
@@ -175,5 +76,126 @@ $(document).ready(function () {
       $(this).toggleClass("d-none");
     });
   });
-  
+
+  // -----Country Code Selection
+  var telInput = $("#mobile_code_login");
+  telInput.intlTelInput({
+    initialCountry: "in",
+    separateDialCode: true,
+    preventInvalidNumbers: true,
+    initialCountry: "eg",
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.9/js/utils.js"
+  });
+
+  $(".login-wrapper .login-form").on("submit", function (e) {
+    e.preventDefault();
+    var inputvalue = $("#mobile_code_login").val();
+    $("#tel").text(inputvalue);
+    if (inputvalue != "" && telInput.intlTelInput("isValidNumber")) {
+      $(this).find(".send-otp").addClass("loading").html("<span class='spinner mr-2'></span><span>Sending OTP...</span>");
+      setTimeout(() => {
+        $(".login-form").hide();
+        $(".otp-content").css("display", "flex");
+        if ($(".otp-content").is(":visible")) {
+          countdown();
+        }
+      }, 1000);
+    } else {
+      $("#error").show().delay(3000).fadeOut();
+    }
+  });
+
+  function countdown() {
+    var seconds = 60;
+    function tick() {
+      var counter = document.getElementById("countdown");
+      seconds--;
+      counter.innerHTML = "00:" + (seconds < 10 ? "0" : "") + String(seconds);
+      if (seconds > 0) {
+        setTimeout(tick, 1000);
+      } else {
+        $(".verify-btn .counter").remove();
+        $('.verify-btn').attr("disabled", true)
+      }
+    }
+    tick();
+  }
+
+  // otp
+  // Restricts input for the set of matched elements to the given inputFilter function.
+  (function ($) {
+    $.fn.inputFilter = function (callback, errMsg) {
+      return this.on("input keydown keyup mousedown mouseup select contextmenu drop focusout", function (e) {
+        if (callback(this.value)) {
+          // Accepted value
+          if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+            $(this).removeClass("input-error");
+            this.setCustomValidity("");
+          }
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          // Rejected value - restore the previous one
+          $(this).addClass("input-error");
+          this.setCustomValidity(errMsg);
+          this.reportValidity();
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          // Rejected value - nothing to restore
+          this.value = "";
+        }
+      });
+    };
+  })(jQuery);
+
+  $(".otp-form input").inputFilter(function (value) {
+    return /^\d*$/.test(value); // Allow digits only, using a RegExp
+  }, "Only digits allowed");
+
+  $(".digit-group")
+    .find("input")
+    .each(function () {
+      $(this).attr("maxlength", 1);
+      $(this).on("keyup", function (e) {
+        // this.value = this.value.replace(/[^0-9]/g, '')
+        var parent = $($(this).parent());
+
+        if (e.keyCode === 8 || e.keyCode === 37) {
+          var prev = parent.find("input#" + $(this).data("previous"));
+          $(this).removeClass("valid");
+
+          if (prev.length) {
+            $(prev).select();
+          }
+        } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+          var next = parent.find("input#" + $(this).data("next"));
+          $(this).addClass("valid");
+
+          if (next.length) {
+            $(next).select();
+          } else {
+            if (parent.data("autosubmit")) {
+              parent.submit();
+            }
+          }
+        }
+      });
+    });
 });
+
+// // otp
+// let digitValidate = function (ele) {
+//   console.log(ele.value);
+//   ele.value = ele.value.replace(/[^0-9]/g, "");
+// };
+
+// let tabChange = function (val) {
+//   let ele = document.querySelectorAll(".otp-form input");
+//   if (ele[val - 1].value != "") {
+//     ele[val].focus();
+//   } else if (ele[val - 1].value == "") {
+//     ele[val - 2].focus();
+//   }
+// };
